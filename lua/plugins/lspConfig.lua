eaff247b36be12c45796e9c8f63de1f6d6087243
Nikita Skhabuitskyi <nikitaskh211@gmail.com>
2025-02-08 23:01:36 +0200

    Removed CMake project builder.

--- lua/plugins/lspConfig.lua
+++ lua/plugins/lspConfig.lua
@@ -10,7 +10,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "asm_lsp", "fortls", "clangd", "cmake", "rust_analyzer" },
+				ensure_installed = { "lua_ls", "asm_lsp", "fortls", "clangd", "rust_analyzer" },
 				automatic_installation = true,
 			})
 		end,
